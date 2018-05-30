7b5f362553595e55204ff10d71bbb41a341aae4e
francesco oliva <olibartfast@gmail.com>
2018-05-31 01:32:45 +0200

    changing contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "//formspree.io/oli_fra@libero.it",
         type: "POST",
         data: {
           name: name,
