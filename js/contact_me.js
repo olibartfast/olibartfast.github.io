652812d49026e410fa2c85b255946dc9977e8b16
francesco oliva <olibartfast@gmail.com>
2018-05-31 02:18:26 +0200

    changing contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,75 +1,35 @@
-$(function() {
+varvar $contactForm = $('#contact-form');
 
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-    preventSubmit: true,
-    submitError: function($form, event, errors) {
-      // additional error messages or events
-    },
-    submitSuccess: function($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
-      // get values from FORM
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
-      $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      $.ajax({
-        url: "//formspree.io/oli_fra@libero.it",
-        type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
-        cache: false,
-        success: function() {
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        complete: function() {
-          setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-          }, 1000);
-        }
-      });
-    },
-    filter: function() {
-      return $(this).is(":visible");
-    },
-  });
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	var $submit = $('input:submit', $contactForm);
+	var defaultSubmitText = $submit.val();
 
-  $("a[data-toggle=\"tab\"]").click(function(e) {
-    e.preventDefault();
-    $(this).tab("show");
-  });
-});
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('#success').html('');
+	$.ajax({
+		url: '//formspree.io/oli_fra@libero.it',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+			$submit.attr('disabled', true).val('Sending message…');
+		},
+		success: function(data) {
+			//$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+			$submit.val('Message sent!');
+			setTimeout(function() {
+				//$('.alert--success').remove();
+				$submit.attr('disabled', false).val(defaultSubmitText);
+			}, 5000);
+		},
+		error: function(err) {
+			//$contactForm.find('.alert--loading').hide();
+			//$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+			$submit.val('Ops, there was an error.');
+			setTimeout(function() {
+				//$('.alert--error').remove();
+				$submit.attr('disabled', false).val(defaultSubmitText);
+			}, 5000);
+		}
+	});
 });
